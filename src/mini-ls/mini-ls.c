85c856b517bd14f542beb81d50a6e28dec0da7c9
Gui <leonsombrio244@gmail.com>
2025-12-08 22:32:30 -0300

    Fixed some commentaries

--- src/mini-ls/mini-ls.c
+++ src/mini-ls/mini-ls.c
@@ -45,41 +45,6 @@ typedef struct {
   int long_format;
 } Options;
 
-// size_t guilen(const char *str) {
-//   size_t counter = 0;
-//   while (str[counter] != '\0') {
-//     ++counter;
-//   }
-//   return counter;
-// }
-//
-// int guicmp(const char *str1, const char *str2) {
-//   while (*str1 != '\0') {
-//     if (*str1 != *str2) {
-//       break;
-//     }
-//     ++str1;
-//     ++str2;
-//   }
-//   return (int)*str1 - (int)*str2;
-// }
-//
-// void guicpy(char *dest, const char *src) {
-//   while (*src != '\0') {
-//     *dest = *src;
-//     ++dest;
-//     ++src;
-//   }
-//   *dest = '\0';
-// }
-//
-// void guicat(char *dest, const char *src) {
-//   while (*dest != '\0') {
-//     ++dest;
-//   }
-//   guicpy(dest, src);
-// }
-//
 void show_help() {
   const char *msg =
       "Mini-LS made by me\n"
